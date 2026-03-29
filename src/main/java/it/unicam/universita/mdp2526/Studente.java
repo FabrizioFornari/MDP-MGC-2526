e2c3de2ffba36948a8f0e0771ff208d7c5042429
Fabrizio Fornari <fabrizio.fornari@unicam.it>
2026-03-29 11:34:04 +0200

    set matricola as final

--- src/main/java/it/unicam/universita/mdp2526/Studente.java
+++ src/main/java/it/unicam/universita/mdp2526/Studente.java
@@ -2,22 +2,20 @@ package it.unicam.universita.mdp2526;
 
 public class Studente extends Persona {
 
-    private int matricola;
+    private final int matricola;
 
     public Studente(String nome, String cognome, int matricola) {
         super(nome, cognome);
-        setMatricola(matricola);
+
+        if (matricola <= 0) {
+            throw new IllegalArgumentException("Matricola non valida");
+        }
+
+        this.matricola = matricola;
     }
 
     public int getMatricola() {
         return matricola;
-    }
-
-    public void setMatricola(int matricola) {
-        if (matricola <= 0) {
-            throw new IllegalArgumentException("Matricola non valida");
-        }
-        this.matricola = matricola;
     }
 
     public void saluta() {
